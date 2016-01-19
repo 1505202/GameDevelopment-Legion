be84bc0d3a6a9931f8570c997f1c74cd33db15f1
longshorts <andrew@andrewoid.co.uk>
2016-01-19 13:20:52 +0000

    Initial upload of Nash's prototype

---
+++ Assets/Script/AIController.cs
@@ -0,0 +1,102 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AIController : MonoBehaviour {
+
+	public GameObject[] legion;
+
+	[SerializeField] private float moveTimer = 0f;
+	[SerializeField] private float nextMoveTime = 0f;
+	[SerializeField] private float maxMoveTime = 10f;
+	[SerializeField] private float minMoveTime = 5f;
+
+	public float wallRayRange = 1f;
+	public Camera cam;
+
+	[SerializeField] private float speed = 7f;
+
+	// Use this for initialization
+	void Start () {
+		if (legion == null)
+			legion = GameObject.FindGameObjectsWithTag ("Legion");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (!GameObject.Find ("GameScript").GetComponent<SpawnController>().start)
+			return;
+
+		moveTimer += Time.deltaTime;
+
+		if (moveTimer >= nextMoveTime || checkWallCollision()) {
+			changeMovement();
+			moveTimer = 0f;
+			nextMoveTime = Random.Range (minMoveTime, maxMoveTime);
+		}
+
+		if (cam != null) {
+			cam.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, cam.transform.position.z);
+		}
+
+		Debug.DrawRay (transform.position, Vector3.up * wallRayRange, Color.green);
+		Debug.DrawRay (transform.position, Vector3.down * wallRayRange, Color.green);
+		Debug.DrawRay (transform.position, Vector3.left * wallRayRange, Color.green);
+		Debug.DrawRay (transform.position, Vector3.right * wallRayRange, Color.green);
+
+	}
+
+	private void changeMovement(){
+		Vector2 direction = getRandomValidMovement ();
+
+		GetComponent<Rigidbody2D> ().velocity = direction * speed;
+	}
+
+	private Vector2 getRandomValidMovement(){
+		int layerMask = 1 << LayerMask.NameToLayer ("Walls");
+		ArrayList directions = new ArrayList ();
+
+		RaycastHit2D upHit = Physics2D.Raycast (transform.position, Vector2.up, wallRayRange, layerMask);
+		RaycastHit2D leftHit = Physics2D.Raycast (transform.position, Vector2.left, wallRayRange, layerMask);
+		RaycastHit2D rightHit = Physics2D.Raycast (transform.position, Vector2.right, wallRayRange, layerMask);
+		RaycastHit2D downHit = Physics2D.Raycast (transform.position, Vector2.down, wallRayRange, layerMask);
+	
+		if (upHit.collider == null)
+			directions.Add (Vector2.up);
+		if (leftHit.collider == null)
+			directions.Add (Vector2.left);
+		if (rightHit.collider == null)
+			directions.Add (Vector2.right);
+		if (downHit.collider == null)
+			directions.Add (Vector2.down);
+
+		//Pick random element from valid directions
+		int i = Random.Range (0, directions.Count);
+		return (Vector2) directions[i];
+	}
+
+	private bool checkWallCollision(){
+		int layerMask = 1 << LayerMask.NameToLayer ("Walls");
+		Vector2 v = GetComponent<Rigidbody2D> ().velocity;
+
+		RaycastHit2D upHit = Physics2D.Raycast (transform.position, Vector2.up, wallRayRange, layerMask);
+		RaycastHit2D leftHit = Physics2D.Raycast (transform.position, Vector2.left, wallRayRange, layerMask);
+		RaycastHit2D rightHit = Physics2D.Raycast (transform.position, Vector2.right, wallRayRange, layerMask);
+		RaycastHit2D downHit = Physics2D.Raycast (transform.position, Vector2.down, wallRayRange, layerMask);
+		
+		if (upHit.collider != null && v.y > 0.1f)
+			return true;
+		else if (leftHit.collider != null && v.x < -0.1f)
+			return true;
+		else if (rightHit.collider != null && v.x > 0.1f)
+			return true;
+		else if (downHit.collider != null && v.y < -0.1f)
+			return true;
+
+		return false;
+	}
+
+	public Camera getCamera(){
+		return cam;
+	}
+}
---
+++ Assets/Script/ControllerScript.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControllerScript : MonoBehaviour, CameraInterface {
+
+	[SerializeField] private float speed = 1;
+	[SerializeField] private KeyCode up;
+	[SerializeField] private KeyCode down;
+	[SerializeField] private KeyCode left;
+	[SerializeField] private KeyCode right;
+
+	public ArrayList cameras = new ArrayList ();
+	public Camera startCamera;
+	bool assimilated = false;
+
+	private Rigidbody2D body;
+
+	// Use this for initialization
+	void Start () {
+		cameras.Add (startCamera);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//Process input
+		Vector2 v = new Vector2 (0, 0);
+		if (Input.GetKey (right)) {
+			v = new Vector2 (v.x + speed, v.y);
+		}
+		if (Input.GetKey (up)) {
+			v = new Vector2 (v.x, v.y + speed);
+		}
+		if (Input.GetKey (left)) {
+			v = new Vector2 (v.x - speed, v.y);
+		}
+		if (Input.GetKey (down)) {
+			v = new Vector2 (v.x, v.y - speed);
+		}
+
+		GetComponent<Rigidbody2D> ().velocity = v;
+
+		if ((gameObject.tag == "RoguePlayer") && !(v.x == 0 && v.y == 0))
+			GameObject.Find ("GameScript").GetComponent<SpawnController> ().start = true;
+
+		if (startCamera != null) {
+			if(!assimilated || gameObject.tag == "Legion") startCamera.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, startCamera.transform.position.z);
+//			else {
+//				startCamera.transform.position = GameObject.Find ("LegionCamera").GetComponent<Camera>().transform.position;
+//			}
+
+		foreach(Camera c in cameras){
+				if(c != null && gameObject.tag == "Legion")
+					c.transform.position = GameObject.Find ("LegionCamera").transform.position;
+		}
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D coll){
+		//Set color of rogue to legion
+		if ((coll.gameObject.tag == "RoguePlayer" || coll.gameObject.tag == "RogueAI") && gameObject.tag == "Legion") {
+			//coll.gameObject.GetComponent<Renderer> ().material = gameObject.GetComponent<Renderer> ().material;
+			gameObject.transform.localScale = new Vector3 (gameObject.transform.localScale.x+0.11f, 1.0f, gameObject.transform.localScale.z+0.11f);
+			if(coll.gameObject.tag == "RogueAI"){
+				cameras.Add (coll.gameObject.GetComponent<AIController>().getCamera ());
+			} else if(coll.gameObject.tag == "RoguePlayer"){
+				cameras.Add (coll.gameObject.GetComponent<ControllerScript>().getCamera ());
+			}
+			assimilated = true;
+
+			Destroy (coll.gameObject);
+
+		}
+
+		Debug.Log ("[" + gameObject.tag + "]" + "Collision with: " + coll.gameObject.tag);
+	}
+
+	public Camera getCamera(){
+		return startCamera;
+	}
+
+}
