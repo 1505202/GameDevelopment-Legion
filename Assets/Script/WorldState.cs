c58a6a3791b18d2d68ca5a6ade9d40da217b79de
longshorts <andrew@andrewoid.co.uk>
2016-01-20 11:25:20 +0000

    Added WorldState for passing around network

---
+++ Assets/Script/WorldState.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class WorldState : MonoBehaviour {
+
+	private List<GameObject> characters = new List<GameObject>();
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GameObject[] legion = GameObject.FindGameObjectsWithTag ("Legion");
+		GameObject[] roguePlayers = GameObject.FindGameObjectsWithTag ("RoguePlayer");
+		GameObject[] rogueAI = GameObject.FindGameObjectsWithTag ("RogueAI");
+
+		foreach (GameObject go in legion)
+			characters.Add (go);
+		foreach (GameObject go in roguePlayers)
+			characters.Add (go);
+		foreach (GameObject go in rogueAI)
+			characters.Add (go);
+	}
+
+	private void clearState(){
+		characters.Clear ();
+	}
+}
