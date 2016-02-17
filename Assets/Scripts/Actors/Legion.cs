6965afda4511794facd3e70911e08119c2326c00
Nelson Costa <1505202@abertay.ac.uk>
2016-02-17 12:53:57 +0000

    removed old prefabs and cleaned up legion code to not expect a camera child object

--- Assets/Scripts/Actors/Legion.cs
+++ Assets/Scripts/Actors/Legion.cs
@@ -7,50 +7,21 @@ using System.Collections.Generic;
 /// </summary>
 [NetworkSettings(channel=1, sendInterval=0)]
 public class Legion : AActor
-{
-//	[Header("Assimilatee Skills")]
-//	[Header("Slow Beam")]
-//
-//	[Header("Tethered Probe")]
-//
-//	[Header("Satellite Probe")]
-//	[SerializeField] private float 
-
-	public enum ELegionState 
-	{ 
-		Controllable,   // Controllable
-		Traversing  	// Traversing Terrain (The Jumps)
-	}
-	private ELegionState legionState = ELegionState.Controllable;
-	
+{	
     [SerializeField] private AController inputController;
 
 	// ComponentCaching
 	private Rigidbody myRigidbody = null;
 	private Transform myTransform = null;
-	private Transform myChildTransform = null;
-
-	// ForceCameraControls
-	private Vector3 cameraRotation = Vector3.zero;
-	private Vector3 cameraOffset = Vector3.zero;
 
 	private void Start()
 	{
+        // Component Caching
 		myRigidbody = GetComponent<Rigidbody>();
-		// Setup new the assimilatedLegionSkillsArray
 		myTransform = GetComponent<Transform>();
 
 		// Add Legion Camera
 		inputController = ControllerManager.Instance.NewController(new JInput( 1 ));
-
-		if (isLocalPlayer) 
-		{
-			myChildTransform = myTransform.GetChild (0);
-			cameraRotation = myChildTransform.rotation.eulerAngles;
-			cameraOffset = myChildTransform.localPosition;
-			myChildTransform.position = myTransform.position + cameraOffset;
-			myChildTransform.gameObject.SetActive(true);
-		}
 	}
 
 	private void Update()
@@ -60,31 +31,12 @@ public class Legion : AActor
 	        return;
 	    }
 
-	    if (legionState == ELegionState.Controllable)
+		myRigidbody.velocity = inputController.MoveDirection() * movementSpeed;
+
+	    if (inputController.MoveDirection() != Vector3.zero)
 	    {
-			myRigidbody.velocity = inputController.MoveDirection() * movementSpeed;
-
-	        if (inputController.MoveDirection() != Vector3.zero)
-	        {
-	            Quaternion lookRotation = Quaternion.LookRotation(inputController.MoveDirection());
-	            myTransform.rotation = Quaternion.Slerp(
-	                myTransform.rotation,
-	                lookRotation,
-	                Time.deltaTime * rotateSpeed);
-
-				if (isLocalPlayer) 
-				{
-	            	myChildTransform.rotation = Quaternion.Euler(cameraRotation);
-	            	myChildTransform.position = myTransform.position + cameraOffset;
-				}
-	        }
-	        return;
-	    }
-
-	    if (legionState == ELegionState.Traversing)
-	    {
-
-	        return;
+	        Quaternion lookRotation = Quaternion.LookRotation(inputController.MoveDirection());
+	        myTransform.rotation = Quaternion.Slerp( myTransform.rotation, lookRotation, Time.deltaTime * rotateSpeed);
 	    }
 	}
 
@@ -98,12 +50,6 @@ public class Legion : AActor
 	}
 
 	#region Properties
-
-	public ELegionState LegionState
-	{
-		get { return legionState; }
-		set { legionState = value; }
-	}
 
 	public AController Controller
 	{
