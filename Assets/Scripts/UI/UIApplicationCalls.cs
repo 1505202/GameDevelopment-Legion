7af33029a2859f887ecec4c287b454d8dbd07129
unknown <1503769@MC10785.uad.ac.uk>
2016-03-17 17:07:17 +0000

    - Added Materials For The Characters - Fixed Tony GIT

--- Assets/Scripts/UI/UIApplicationCalls.cs
+++ Assets/Scripts/UI/UIApplicationCalls.cs
@@ -1,17 +1,21 @@
 ﻿using UnityEngine;
+using System.Collections;
+//using UnityEngine.SceneManagement;
 
-using UnityEngine.SceneManagement;
 
+// Temporary Left Without SceneManagment Due To Tony's Computer Not Having 5.3
 public class UIApplicationCalls : MonoBehaviour 
 {
     public void LoadLevel(int index)
     {
-        SceneManager.LoadScene(index);
+		Application.LoadLevel (index);
+        //SceneManager.LoadScene(index);
     }
 
     public void LoadLevel(string levelName)
     {
-        SceneManager.LoadScene(levelName);
+		Application.LoadLevel (levelName);
+        //SceneManager.LoadScene(levelName);
     }
 
     public void QuitApplication()
