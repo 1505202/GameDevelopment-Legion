75785d6ea0adf4ef5963b3e7937fc5774810342a
unknown <1001089@MC10787.uad.ac.uk>
2016-03-18 16:55:20 +0000

    - Added Particle Systems

---
+++ Assets/Scripts/ScrollingTexture.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScrollingTexture : MonoBehaviour {
+    MeshRenderer Mesh;
+    public Vector2 Speed;
+
+	// Use this for initialization
+	void Start () {
+        Mesh = GetComponent<MeshRenderer>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Mesh.material.SetTextureOffset( "_MainTex", Speed * Time.time);
+	}
+}
